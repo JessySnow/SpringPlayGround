d51011ee01286c2f277c3c556e4591be5c443bf6
thuban <jessysnow12345@gmail.com>
2022-09-26 22:08:59 +0800

    Modify

--- simple-mvc-anno/src/main/java/org/play/controller/BaseController.java
+++ simple-mvc-anno/src/main/java/org/play/controller/BaseController.java
@@ -1,2 +1,20 @@
-package org.play.controller;public class BaseController {
+package org.play.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("base.do")
+public class BaseController {
+    // 两个不同请求方式，返回不同的对象
+    @RequestMapping(method = RequestMethod.GET)
+    public String testGET(){
+        return "testGET";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String testPOST(){
+        return "testPOST";
+    }
 }
--- simple-mvc-anno/src/main/webapp/WEB-INF/form.jsp
+++ simple-mvc-anno/src/main/webapp/WEB-INF/form.jsp
@@ -7,10 +7,12 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form action="/simple_mvc_anno/base.do" method="post">
+        <input type="submit" value="submit">
+    </form>
+</body>
 </html>
